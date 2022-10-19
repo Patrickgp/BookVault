26d2b6ba405793149dc76ac863dd79bc44905051
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-10-19 18:46:01 -0400

    completed SavedBooks.js refactor. MVP complete

--- server/server.js
+++ server/server.js
@@ -23,13 +23,13 @@ app.use(express.json());
 
 // UNCOMMENT AFTER APOLLO TESTING
 // Serve up static assets
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client/build")));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
